88f2e033df93b5a96f353454ae911c94d67b4ce5
shaashwathsivakumar <146679078+shaashwathsivakumar@users.noreply.github.com>
2025-05-07 00:40:59 -0700

    Adding Files

---
+++ simple_z.py
@@ -0,0 +1,21 @@
+from pettingzoo.mpe import simple_tag_v3
+
+env = simple_tag_v3.parallel_env(render_mode="human")
+observations, infos = env.reset()
+i = 0
+
+while env.agents:
+    
+    # this is where you would insert your policy
+    actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+
+    observations, rewards, terminations, truncations, infos = env.step(actions)
+    print(f"i: {i}, obs: {observations}")
+    i += 1
+    
+env.close()
+
+#Step 1: Know how to interpret each state
+#Step 2: Know how to interpret each action
+#Step 3: Create a policy that takes in the state and outputs an action
+#Step 4: Put this all in a GitHub repo so that everyone can have full access to it.
