a4b85ab01dc6e0cacb9bf13b5c708947cf1fe7d3
jliucla <jasonlliu@ucla.edu>
2025-05-14 16:19:19 -0700

    test.py is mostly completed for 1 iteration example

--- test.py
+++ test.py
@@ -1,12 +1,54 @@
+import torch
 from pettingzoo.mpe import simple_tag_v3
+from train_MADDPG import Actor, Critic, ReplayBuffer
+import torch.nn as nn
+
+
+soft = nn.Softmax(dim=0)
 
 env = simple_tag_v3.parallel_env(render_mode="human", max_cycles=1000)
+# just get the environment shape
 observations, infos = env.reset()
+adversary_list = env.agents[0:-1]
+actors = [Actor(len(observations[adversary]), 5) for adversary in adversary_list]
+critics = [Critic(len(env.state()), env.action_space(adversary).n) for adversary in adversary_list]
+actor_optimizers = [torch.optim.Adam(actor.parameters(), lr=0.001) for actor in actors]
+critic_optimizers = [torch.optim.Adam(critic.parameters(), lr=0.001) for critic in critics]
+D = ReplayBuffer(100)
+gamma = 0.9
+minibatch_size = 1
 
-# this works
-print(env.num_agents)
+# iterate over this many episodes
+observations, infos = env.reset()
+# iterate over this many steps
+state = env.state()
+actions = {"agent_0": env.action_space("agent_0").sample()}
+for i, adversary in enumerate(adversary_list):
+    actions[adversary] = torch.multinomial(soft(actors[i].forward(observations[adversary])), num_samples=1).item()
+next_observations, rewards, terminations, truncations, infos = env.step(actions)
+next_state = env.state()
+D.push(state, actions, rewards, next_state, observations)
+observations = next_observations
 
-# but this doesn't
-print(env.unwrapped.num_adversaries)
+if len(D.buffer) >= minibatch_size:
+    for i, adversary in enumerate(adversary_list):
+        minibatch = D.sample(minibatch_size)
+        targets = []
+        evals = []
+        for s_state, s_actions, s_rewards, s_next_state, s_observations in minibatch:
+            s_next_actions = {"agent_0": env.action_space("agent_0").sample()}
+            for j, each_adversary in enumerate(adversary_list):
+                obs_tensor = torch.tensor(s_observations[each_adversary], dtype=torch.float32)
+                s_next_actions[each_adversary] = torch.multinomial(soft(actors[j].forward(obs_tensor)), num_samples=1).item()
+            reward = torch.tensor([s_rewards[adversary]], dtype=torch.float32)
+            targets.append(reward + gamma * critics[i].forward(s_next_state, s_next_actions).detach())
+            evals.append(critics[i].forward(s_state, s_actions))
 
-# so that means the attributes don't come from the scenario class, but where does num_agents come from?
+        loss = torch.nn.functional.mse_loss(torch.stack(targets).squeeze(), torch.stack(evals).squeeze())
+        critic_optimizers[i].zero_grad()
+        loss.backward()
+        critic_optimizers[i].step()
+
+
+
+
--- train_MADDPG.py
+++ train_MADDPG.py
@@ -11,15 +11,15 @@ from pettingzoo.mpe import simple_tag_v3
 
 
 class Actor(nn.Module):
-    def __init__(self, state_size, action_size):
+    def __init__(self, obs_size, action_size):
         super(Actor, self).__init__()
-        self.fc1 = nn.Linear(state_size, 64)
+        self.fc1 = nn.Linear(obs_size, 64)
         self.fc2 = nn.Linear(64, 64)
         # Give the desired size for the output layer
         self.fc3 = nn.Linear(64, action_size)
 
-    def forward(self, state):
-        x = torch.relu(self.fc1(torch.tensor(state)))
+    def forward(self, obs):
+        x = torch.relu(self.fc1(torch.tensor(obs)))
         x = torch.relu(self.fc2(x))
         # Obtain the action probabilities
         action_probs = torch.softmax(self.fc3(x), dim=-1)
@@ -27,14 +27,16 @@ class Actor(nn.Module):
 
 
 class Critic(nn.Module):
-    def __init__(self, state_size):
+    def __init__(self, state_size, num_actions):
         super(Critic, self).__init__()
-        self.fc1 = nn.Linear(state_size, 64)
+        self.fc1 = nn.Linear(state_size + num_actions, 64)
         # Fill in the desired dimensions
         self.fc2 = nn.Linear(64, 1)
 
-    def forward(self, state):
-        x = torch.relu(self.fc1(torch.tensor(state)))
+    def forward(self, state, actions):
+        s = torch.tensor(state)
+        a = torch.tensor(actions)
+        x = torch.relu(self.fc1(torch.cat((s, a), dim=-1)))
         # Calculate the output value
         value = self.fc2(x)
         return value
@@ -44,26 +46,29 @@ class ReplayBuffer:
     def __init__(self, capacity):
         self.buffer = deque(maxlen = capacity)
 
-    def push(self, obs, actions, rewards, next_obs, done):
-        data = (obs, actions, rewards, next_obs, done)
+    def push(self, state, actions, rewards, next_state, obs):
+        data = (state, actions, rewards, next_state, obs)
         self.buffer.append(data)
 
     def sample(self, batch_size):
         return random.sample(self.buffer, batch_size)
 
-
-def train(environment, max_episode_count, max_episode_length):
-    env = simple_tag_v3.parallel_env(render_mode="human", max_cycles=1000)
+'''
+def train(environment, max_episode_count, max_episode_length, max_capacity):
+    env = simple_tag_v3.parallel_env(render_mode="human", max_cycles=max_episode_length)
     observations, infos = env.reset()
     # create a tuple of agent networks
-    actors = [Actor() for i in range(env.num_agents-1)]
-    critics = [Critic() for i in range(env.num_agents-1)]
-    for episode in range(max_episode_count):
+    # We need to find the actual state size and action size (to replace 16 & 5)
+    actors = [Actor(16, 5) for i in range(env.num_agents-1)]
+    critics = [Critic(16) for i in range(env.num_agents-1)]
+    D = ReplayBuffer(max_capacity)
+    while env.agents:
         # initialize random process for action exploration
-        observations, info = env.reset()
         # receive initial state x
+        observations, info = env.reset()
         for t in range(max_episode_length):
-            for agent in agents:
+            for actor in actors:
+                actor.forward()
                 # select action a_i w.r.t. current action policy + exploration
             # execute actions
             # observe reward and new state
@@ -75,3 +80,4 @@ def train(environment, max_episode_count, max_episode_length):
                 # update critic by minimizing loss
                 # update actor using sampled policy gradient
             # update target network parameters for all agents
+'''
